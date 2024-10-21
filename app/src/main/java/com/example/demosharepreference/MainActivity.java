707f49e596263d1c149bf66175cfb3c86a94ba2d
binhabc <dothaibinh021218@gmail.com>
2024-10-21 08:43:04 +0700

    demo preference

---
+++ app/src/main/java/com/example/demosharepreference/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.demosharepreference;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView v1,v2,v3,v4,v5;
+    private TextView txtdiem;
+    private int diemso;
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        sharedPreferences= getSharedPreferences("diem", MODE_PRIVATE);
+        diemso = sharedPreferences.getInt("diemso", 0); // Lấy giá trị "diemso" từ SharedPreferences
+
+        // Khởi tạo các nút và TextView
+        v1 = findViewById(R.id.v1);
+        v2 = findViewById(R.id.v2);
+        v3 = findViewById(R.id.v3);
+        v4 = findViewById(R.id.v4);
+        v5 = findViewById(R.id.v5);
+        txtdiem = findViewById(R.id.diem);
+
+        // Hiển thị điểm hiện tại khi khởi động
+        txtdiem.setText("Điểm: " + diemso);
+
+        // Khởi tạo sự kiện click
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                diemso += 10;
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt("diemso", diemso);
+                editor.commit();
+                txtdiem.setText("Điểm: " + diemso); // Cập nhật TextView
+            }
+        };
+
+        // Áp dụng sự kiện click cho các nút
+        v1.setOnClickListener(listener);
+        v2.setOnClickListener(listener);
+        v3.setOnClickListener(listener);
+        v4.setOnClickListener(listener);
+        v5.setOnClickListener(listener);
+    }
+}
